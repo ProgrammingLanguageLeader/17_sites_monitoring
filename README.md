45f6e5c23c09b9f0783577cce0298acefaa989ca
ProgrammingLanguageLeader <vip.shoroch@gmail.com>
2017-10-18 13:54:07 +0300

    Implemented check_sites_health.py and README.md. Added requirements.txt

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+A script enabling you to check sites health. Site is healthy if it responses HTTP status 200 and its domain name expires in more than 1 month.
+
+# How to install
+
+Python 3 should be already installed. Then use pip to install dependencies:
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+It's recommended to use [virtual environment](https://docs.python.org/3/tutorial/venv.html) for better isolation.
+
+# How to use
+
+A launching on Linux:
+```bash
+python check_sites_health.py urls.txt # alternatively try python3
+http://google.com is healthy
+http://vk.com is healthy
+http://yandex.ru is healthy
+http://rtthtrhrhfhgfhd.org: unable to fetch expiration date
+http://rambler.ru is healthy
+localhost: unable to fetch expiration date
+127.0.0.1: unable to fetch expiration date
+http://qwerty.com is healthy
+http://127.0.0.1: unable to fetch expiration date
+```
+The launching on Windows is similar.
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,78 @@
-def load_urls4check(path):
-    pass
+import whois
+import whois.parser
+import requests
+import requests.exceptions
+import argparse
+from datetime import datetime
+from datetime import timedelta
 
-def is_server_respond_with_200(url):
-    pass
+
+def load_urls(path):
+    try:
+        urls = []
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file.readlines():
+                urls.append(line.strip())
+        return urls
+    except IOError:
+        return None
+
+
+def get_server_status(url):
+    try:
+        return requests.get(url).status_code
+    except requests.exceptions.RequestException:
+        return None
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    try:
+        response = whois.whois(domain_name)
+    except whois.parser.PywhoisError:
+        return None
+    try:
+        return response.expiration_date[0]
+    except TypeError:
+        return response.expiration_date
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='A script enabling you to check sites health. Site is '
+                    'healthy if it responses HTTP status 200 and its domain '
+                    'name expires in more than 1 month'
+    )
+    parser.add_argument(
+        'urls_path',
+        help='A path to the file which contains urls to check'
+    )
+    return parser.parse_args()
+
+
+def check_health(url):
+    days_minimum = 31
+    ok_status = 200
+    expiration_date = get_domain_expiration_date(url)
+    if not expiration_date:
+        return '{}: unable to fetch expiration date'.format(url)
+    days_to_expiration = expiration_date - datetime.today()
+    server_status = get_server_status(url)
+    if not server_status:
+        return '{}: unable to connect to the server. ' \
+               'Check the URL, please'.format(url)
+    expiration_date_is_ok = days_to_expiration >= timedelta(days_minimum)
+    if server_status == ok_status and expiration_date_is_ok:
+        return '{} is healthy'.format(url)
+    if server_status != ok_status:
+        return '{} server does not response 200 HTTP code'.format(url)
+    if not expiration_date_is_ok:
+        return '{} domain expires in less than 1 month'.format(url)
+
 
 if __name__ == '__main__':
-    pass
+    args = parse_args()
+    urls = load_urls(args.urls_path)
+    if not urls:
+        exit('Unable to read {}'.format(args.urls_path))
+    for url in urls:
+        print(check_health(url))
